67b2deffd7cd24c9d99fe00aedff8edf780a51da
Gustav <mgustav.penting@gmail.com>
2023-06-08 08:30:14 +0700

    ad

--- Admincoy.java
+++ Admincoy.java
@@ -249,41 +249,38 @@ public class Admincoy extends javax.swing.JFrame {
         // TODO add your handling code here for jMenu3
         Map<String, Accounts> daftarKaryawan = Login.loadAccountsFromFile();
 
-    // Specify the file path where you want to save the data
-    String filePath = "Daftar Karyawan.txt";
+        // Specify the file path where you want to save the data
+        String filePath = "Daftar Karyawan.txt";
 
-    // Create a FileWriter to write data to the file
-    FileWriter writer = new FileWriter(filePath);
+        // Create a FileWriter to write data to the file
+        FileWriter writer = new FileWriter(filePath);
 
-    // Iterate over the map entries
-    for (Map.Entry<String, Accounts> entry : daftarKaryawan.entrySet()) {
-        String key = entry.getKey();
-        Accounts accounts = entry.getValue();
+        // Iterate over the map entries
+        for (Map.Entry<String, Accounts> entry : daftarKaryawan.entrySet()) {
+            String key = entry.getKey();
+            Accounts accounts = entry.getValue();
 
-        // Write the data to the file
-        writer.write("Username: " + key + "\n");
-        writer.write("Nama:\t" + accounts.getKaryawan().getNama() + "\n");
-        writer.write("Alamat:\t" + accounts.getKaryawan().getAlamat() + "\n");
-        writer.write("Telepon:\t" + accounts.getKaryawan().getTelepon() + "\n");
-        writer.write("Jenis Kelamin:\t" + accounts.getKaryawan().getJenisKelamin() + "\n");
-        writer.write("Kategori:\t" + accounts.getKaryawan().getKategoriKaryawan() + "\n");
-        writer.write("Gaji:\t" + df.format(accounts.getKaryawan().getGaji()) + "\n");
-        if (!(accounts.getKaryawan() instanceof Admin)) {
-            writer.write("Biaya:\t" + df.format(accounts.getKaryawan().getBiaya()) + "\n");
+            // Write the data to the file
+            writer.write("Username: " + key + "\n");
+            writer.write("Nama:\t" + accounts.getKaryawan().getNama() + "\n");
+            writer.write("Alamat:\t" + accounts.getKaryawan().getAlamat() + "\n");
+            writer.write("Telepon:\t" + accounts.getKaryawan().getTelepon() + "\n");
+            writer.write("Jenis Kelamin:\t" + accounts.getKaryawan().getJenisKelamin() + "\n");
+            writer.write("Kategori:\t" + accounts.getKaryawan().getKategoriKaryawan() + "\n");
+            writer.write("Gaji:\t" + df.format(accounts.getKaryawan().getGaji()) + "\n");
+            if (!(accounts.getKaryawan() instanceof Admin)) {
+                writer.write("Biaya:\t" + df.format(accounts.getKaryawan().getBiaya()) + "\n");
+            }
+            
+
+            // Add any additional formatting or data you want to write
+            // ...
+
+            writer.write("\n"); // Add a newline after each entry
         }
-        
 
-        // Add any additional formatting or data you want to write
-        // ...
-
-        writer.write("\n"); // Add a newline after each entry
-    }
-
-    // Close the FileWriter
-    writer.close();
-}
-
-        
+        // Close the FileWriter
+        writer.close();
     }
     
     private void jMenu4MouseClicked(java.awt.event.MouseEvent evt) {
